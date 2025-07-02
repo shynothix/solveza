b137d8fe5c9451feb8fc22aec56da34856bc527f
shinon <sinkawa291@icloud.com>
2025-07-02 22:33:57 +0900

    chore(api): spotlessを導入
    フォーマッターを導入することでコードスタイルを統一させたい
    ひとまずは一般的な設定で様子見

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -4,6 +4,21 @@ plugins {
     id("io.spring.dependency-management") version "1.1.7"
     id("org.graalvm.buildtools.native") version "0.10.6"
     id("org.asciidoctor.jvm.convert") version "3.3.2"
+    id("com.diffplug.spotless") version "7.0.4"
+}
+
+spotless {
+    java {
+        googleJavaFormat()
+        importOrder()
+        removeUnusedImports()
+        trimTrailingWhitespace()
+        endWithNewline()
+    }
+    kotlinGradle {
+        ktfmt()
+        ktlint()
+    }
 }
 
 group = "com.shinkaji"
